c7095a9c33a6e866394a00f3faab1ec7c0cee1b2
TomRiddle67 <courage0047@gmail.com>
2026-03-31 18:20:04 +0100

    Priority Functionality

--- task.py
+++ task.py
@@ -1,10 +1,14 @@
 tasks = []
 #add_task
 
-def add_task (title):
-    task = {"title": title, "done": False}
+def add_task (title, priority ="meduim"):
+    task = {
+        "title": title,
+        "done": False,
+        "priority": priority # "high" "meduim" "low"
+        }
     tasks.append(task)
-    print (f"Added: {title}")
+    print (f"Added:[{priority}]: {title}")
 
 add_task("Pickup package")
 add_task("Buy Fruits")
@@ -16,10 +20,12 @@ def view_tasks():
         return
     for i, task in enumerate(tasks):
         status = "[x]" if task ["done"] else "[]"
-        print(f" {i}: {status} {task['title']}")
+        pri = task.get("priority","meduim")
+        print(f" {i}: {status} [{pri}] {task['title']}")
 
 view_tasks()
 
+#delete_task
 def delete_task(index):
     if index < 0 or index >= len(tasks):
         print("Invalid index!")
