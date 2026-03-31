5d1e793e25293d870085b61a1aec9e932f3a0dfa
TomRiddle67 <courage0047@gmail.com>
2026-03-31 17:59:04 +0100

    Delete task functionality

--- task.py
+++ task.py
@@ -7,6 +7,7 @@ def add_task (title):
     print (f"Added: {title}")
 
 add_task("Pickup package")
+add_task("Buy Fruits")
 
 #view_task
 def view_tasks():
@@ -18,3 +19,11 @@ def view_tasks():
         print(f" {i}: {status} {task['title']}")
 
 view_tasks()
+
+def delete_task(index):
+    if index < 0 or index >= len(tasks):
+        print("Invalid index!")
+        return
+    removed= tasks.pop(index)
+    print(f"Deleted: {removed['title']}")
+delete_task (1)
