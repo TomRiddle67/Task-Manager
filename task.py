b541d95562cbff88469d5318797270479adb73d5
TomRiddle67 <courage0047@gmail.com>
2026-03-31 17:27:57 +0100

    View task funtionality

--- task.py
+++ task.py
@@ -1,9 +1,20 @@
 tasks = []
-"add task"
+#add_task
 
 def add_task (title):
     task = {"title": title, "done": False}
     tasks.append(task)
     print (f"Added: {title}")
-    
+
 add_task("Pickup package")
+
+#view_task
+def view_tasks():
+    if len(tasks)== 0:
+        print("no tasks yet!")
+        return
+    for i, task in enumerate(tasks):
+        status = "[x]" if task ["done"] else "[]"
+        print(f" {i}: {status} {task['title']}")
+
+view_tasks()
